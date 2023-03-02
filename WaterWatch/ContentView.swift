aff2b0129579dec90c913efd20f74aafc02bd318
Nathan Aronson <njaronson421@gmail.com>
2023-03-02 09:57:43 -0500

    Initial Commit

---
+++ WaterWatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterWatch
+//
+//  Created by Nathan Aronson (student LM) on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterWatch/WaterWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterWatchApp.swift
+//  WaterWatch
+//
+//  Created by Nathan Aronson (student LM) on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
