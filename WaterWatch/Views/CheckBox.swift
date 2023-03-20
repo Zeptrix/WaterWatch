cd0e4decd95529a8735512492650b1f4f29b701c
Ori123 <ori.orbach@koheletyeshiva.org>
2023-03-20 09:26:49 -0400

    Added user info and survey view

---
+++ WaterWatch/Views/CheckBox.swift
@@ -0,0 +1,82 @@
+//
+//  CheckBox.swift
+//  WaterWatch
+//
+//  Created by Ori Orbach (student LM) on 3/13/23.
+//
+import SwiftUI
+struct CheckBox: View {
+    
+    @State var isChecked1:Bool = false
+    @State var isChecked2:Bool = false
+    @State var isChecked3:Bool = false
+    @State var StringImage : String = ""
+    
+    var title1:String
+    var title2:String
+    
+    func toggle1(){
+        isChecked1 = !isChecked1
+        if StringImage.count < 10{
+            StringImage += title1
+        }
+        else if isChecked1 == true && isChecked2 == false{
+            StringImage = title1
+        }
+        else{
+            StringImage = title2
+        }
+   }
+    func toggle2(){
+        isChecked2 = !isChecked2
+        if StringImage.count < 10{
+            StringImage += title2
+        }
+        else if isChecked1 == false && isChecked2 == true{
+            StringImage = title2
+        }
+        else{
+            StringImage = title1
+        }
+   }
+   var body: some View {
+       
+       Button(action: toggle1){
+           HStack{
+               Text(title1)
+               Image(systemName: isChecked1 ? "checkmark.square": "square")
+           }
+       }
+     
+       Button(action: toggle2){
+           HStack{
+               Text(title2)
+               Image(systemName: isChecked2 ? "checkmark.square": "square")
+           }
+       }
+       
+       if isChecked1 == true{
+           Image(StringImage)
+       }
+       else if isChecked2 == true{
+           Image(StringImage)
+       }
+      
+     
+   }
+}
+struct CheckBox_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckBox(title1: "", title2: "")
+}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ WaterWatch/Views/SurveyView.swift
@@ -0,0 +1,72 @@
+//
+//  SurveryView.swift
+//  WaterWatch
+//
+//  Created by Ori Orbach (student LM) on 3/8/23.
+//
+import SwiftUI
+import Foundation
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
+struct SurveryView: View {
+    
+    @StateObject var userInfo: UserInfo = UserInfo()
+    
+    
+    @State var textFieldAge: String = ""
+    @State var weight: Double = 0.0
+    @State var activity: Double = 0.0
+    
+    //find sliders for weight and activity
+    
+    var body: some View {
+        
+        NavigationView{
+            VStack{
+                TextField("Enter your age", text: $textFieldAge)
+                    .padding()
+                    .background(Color.gray.opacity(0.3).cornerRadius(10))
+                    .foregroundColor(.red)
+                    .font(.headline)
+                
+                Button(action:{
+                    userInfo.age = Double(textFieldAge) ?? 0.0
+                }, label: {
+                    Text("Save".uppercased())
+                        .frame(maxWidth: . infinity)
+                        .padding()
+                        .background(Color.blue.cornerRadius(10))
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    
+                })
+                Spacer()
+                //slider
+                Slider(
+                    value: $userInfo.weight,
+                    in: 0...400, step: 1)
+                Text("\(userInfo.weight, specifier: "%.0001f") weight in pounds")
+                
+                
+                Slider(
+                    value: $userInfo.activity,
+                    in: 0...360, step: 15)
+                Text("\(userInfo.activity, specifier: "%.0001f") avg daily minutes of actvity")
+                
+                
+            }
+        }
+        
+    }
+ 
+    
+}
+struct SurveryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SurveryView()
+    }
+}
+
+
