6f5d343f8e4a94fba188007ca53a73582858baf9
Nathan Aronson <njaronson421@gmail.com>
2023-03-20 08:51:38 -0400

    Finished Firebase

--- WaterWatch/Views/HomeView.swift
+++ WaterWatch/Views/HomeView.swift
@@ -6,13 +6,31 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct HomeView: View {
     
     @Binding var viewState: ViewState
+    @EnvironmentObject var userInfo: UserInfo
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {
+                try! Auth.auth().signOut()
+                userInfo.loggedIn = false
+                viewState = .authentication
+            }) {
+                Text("Log Out")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 300, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(15.0)
+                    .shadow(radius: 10.0, x: 20, y: 10)
+            }
+        }
     }
 }
 
