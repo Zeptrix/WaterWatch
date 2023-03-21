de012a3376f181cd2676bc133e079aaa8d29d3c0
Brendan McGrew <s016752@students.lmsd.org>
2023-03-21 10:30:22 -0400

    home view and assets for it

--- WaterWatch/Views/HomeView.swift
+++ WaterWatch/Views/HomeView.swift
@@ -10,12 +10,70 @@ import Firebase
 import FirebaseAuth
 
 struct HomeView: View {
-    
+    @Binding var water_mL: Int
+    var waterRequirement_mL = 300
+    var drinkSize_mL = 100
+
     @Binding var viewState: ViewState
     @EnvironmentObject var userInfo: UserInfo
     
     var body: some View {
+        ZStack{
+            if Double(waterRequirement_mL-water_mL) <= 100{
+                Image("wet")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+            } else if Double(waterRequirement_mL-water_mL) <= 200{
+                Image("dirt")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+            } else if Double(waterRequirement_mL-water_mL) <= 300{
+                Image("dry")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .aspectRatio(contentMode: .fill)
+            }
+        
         VStack {
+            Spacer()
+                  if waterRequirement_mL-water_mL > 0{
+                      Text("\(waterRequirement_mL-water_mL) mL remaining today")
+                          .padding()
+                  } else if waterRequirement_mL-water_mL <= 0{
+                      Text("All necessary water has been consumed!")
+                          .padding()
+                  }
+                  
+                  Spacer()
+                  Button{
+                      water_mL += drinkSize_mL
+                  } label: {
+                      if waterRequirement_mL-water_mL > 0{
+                          ZStack{
+                              Rectangle()
+                                  .foregroundColor(Color.green)
+                                  .cornerRadius(10)
+                                  .padding()
+                              Text("Drink")
+                                  .foregroundColor(Color.white)
+                          }
+                      } else {
+                          ZStack{
+                              Rectangle()
+                                  .foregroundColor(Color.gray)
+                                  .cornerRadius(10)
+                                  .padding()
+                              Text("Drink")
+                                  .foregroundColor(Color.white)
+                          }
+                          
+                      }
+                  }
+                  Spacer()
+        }}
+
             Button(action: {
                 try! Auth.auth().signOut()
                 userInfo.loggedIn = false
