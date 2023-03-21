6a32bb51dda8cfcdfa1723fb1d40e0af51a4e06a
Brendan McGrew <s016752@students.lmsd.org>
2023-03-21 10:41:16 -0400

    weatherAPI

--- WaterWatch/Views/ContentView.swift
+++ WaterWatch/Views/ContentView.swift
@@ -15,8 +15,23 @@ struct ContentView: View {
     
     @State var viewState: ViewState = ViewState.authentication
     @EnvironmentObject var userInfo: UserInfo
-    
+    @StateObject var data = FetchData()
+    var userLatitude: String{
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
     var body: some View {
+     
+  
+     //make sure you use await data.getData(). for example:
+        //            Text("Humidity: \(data.response.current.humidity)")
+        //                .task{
+        //                    await data.getData()
+        //                }
+
+
         if viewState == .authentication && !userInfo.loggedIn {
             AuthenticationView(viewState: $viewState)
         } else if viewState == .login && !userInfo.loggedIn {
